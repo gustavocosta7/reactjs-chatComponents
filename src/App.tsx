1bba3eda6eeef919440b9358e5e4a75dbea046a8
GUstavo costa <gustavocosta@gmail.com>
2020-06-19 01:08:41 -0400

    progressBarComponent, tentando setar valor de porcentagem dinâmico

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ChatBubbleComponent from './components/vectors/chatBubble/ChatBubbleComponent';
+import ProgressBarCircle from './components/vectors/progressBarCircle/ProgressBarCircleComponent';
+
 
 function App() {
+  const ProgressRef = useRef();
+
   return (
     <div className="App">
       <ChatBubbleComponent text={"texto"}></ChatBubbleComponent>
+      <ProgressBarCircle childRef={ProgressRef} porcentagem={100}></ProgressBarCircle>
     </div>
   );
 }
