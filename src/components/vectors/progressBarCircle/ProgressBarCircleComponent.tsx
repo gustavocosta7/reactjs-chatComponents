12d162ad3d431ba83678868ae725da5e36694130
GUstavo costa <gustavocosta@gmail.com>
2020-06-20 15:11:50 -0400

    Porcentagem dinâmiica circle progress bar

--- src/components/vectors/progressBarCircle/ProgressBarCircleComponent.tsx
+++ src/components/vectors/progressBarCircle/ProgressBarCircleComponent.tsx
@@ -1,34 +1,34 @@
-import React, {useRef, useImperativeHandle, forwardRef} from 'react'
+import React, { useRef, useImperativeHandle, useEffect, useState } from 'react'
 import './progressBarCircle.css'
 interface IProps {
     width?: number,
     heigth?: number,
-    porcentagem?: number,
-    childRef: any
-} 
+    percentage?: number,
+}
 
-const setPx  = (porcentagem: number, circleRef: any, divRef: any) => {
-    
+const setPx = (percentage: number, circleRef: any, divRef: any) => {
+
     const radius = circleRef.current.getAttribute('r');
-    const piCircle = Math.PI * parseFloat(radius) *2;
-    if (porcentagem < 0) { porcentagem = 0;}
-    if (porcentagem > 100) { porcentagem = 100;}
+    const piCircle = Math.PI * parseFloat(radius) * 2;
+    if (percentage < 0) { percentage = 0; }
+    if (percentage > 100) { percentage = 100; }
 
-    const pct = ((100-porcentagem)/100)*piCircle;
-     
+    const pct = ((100 - percentage) / 100) * piCircle;
+
     circleRef.current.style.strokeDashoffset = pct;
 
-} 
+}
 
-const  ProgressBarCircle = forwardRef((props: any, ref: any) => {
+const ProgressBarCircle = ({ width = 30, heigth = 30, percentage = 0}: IProps) => {
     const circleRef = useRef(null);
     const divRef = useRef(null);
 
-    useImperativeHandle(ref, () => {
-        setPx(props.porcentagem, circleRef, divRef)
-     });
 
-    return(
+    useEffect(() => {
+        setPx(percentage,circleRef, divRef)
+    })
+
+    return (
         <div id="cont" data-pct="100" ref={divRef}>
             <svg id="svg" width="200" height="200" version="1.1" xmlns="http://www.w3.org/2000/svg">
                 <circle r="90" cx="100" cy="100" fill="transparent" strokeDasharray="565.48" strokeDashoffset="0"></circle>
@@ -36,6 +36,6 @@ const  ProgressBarCircle = forwardRef((props: any, ref: any) => {
             </svg>
         </div>
     )
-})
+}
 
 export default ProgressBarCircle;
